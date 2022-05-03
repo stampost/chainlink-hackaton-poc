7c7ba1191de3a332d278e9b85a8199300da0ac9a
Maxim Manylov <manylov@gmail.com>
2022-05-03 10:12:22 +0700

    add STAMP token contract

--- contracts/contracts/Stampost.sol
+++ contracts/contracts/Stampost.sol
@@ -1,15 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+
 import 'hardhat/console.sol';
 
 contract Stampost {
-
 
   struct PublicKey {
    string key;
   }
 
+  enum PublicKeyRequestStatus { PENDING, ACCEPTED, DECLINED }
+
+  struct PublicKeyRequest {
+    uint256 timestamp;
+    address from;
+    uint256 stamps;
+    PublicKeyRequestStatus status;
+  }
+
   mapping (address =>  PublicKey) public publicKeys;
+
 
   
   function requestPublicKey(
@@ -19,6 +30,7 @@ contract Stampost {
     string calldata _publicKey
   ) public {
     publicKeys[msg.sender].key = _publicKey;
+
   }
 
   function getAcceptedPublicKey(address _recepient) public view returns (string memory) {
